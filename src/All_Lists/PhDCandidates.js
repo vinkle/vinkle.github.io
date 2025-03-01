3ff430e906c9d81c57c44252ef1c1ac229526164
vinkle <vinkle.kumar@gmail.com>
2025-03-01 15:43:59 +0100

    v0

--- src/All_Lists/PhDCandidates.js
+++ src/All_Lists/PhDCandidates.js
@@ -8,7 +8,8 @@ import Shi_li from "../assets/phds/Shi_li.jpeg";
 export const phdCandidates = [
     {
       name: 'Idris Hamoud',
-      link: 'https://www.linkedin.com/in/idris-hamoud-a147a3140/',
+      linkedin_link: 'https://www.linkedin.com/in/idris-hamoud-a147a3140/',
+      scholar_link: 'https://scholar.google.fr/citations?user=Af14BLoAAAAJ&hl=en&oi=ao',
       period: '[2020 -- ongoing]',
       thesis: 'Thesis on "Self-supervised structured learning for robotic operating room workflow monitoring."',
       image: idris_hamoud,
@@ -17,30 +18,33 @@ export const phdCandidates = [
     },
     {
       name: 'Kun Yuan',
-      link: 'https://www.linkedin.com/in/kun-yuan-b2425219b/',
+      linkedin_link: 'https://www.linkedin.com/in/kun-yuan-b2425219b/',
+      scholar_link: 'https://scholar.google.com/citations?user=zId4EqoAAAAJ',
       period: '[2022 -- ongoing]',
       thesis: 'Thesis on "Large-scale multi-modality learning for surgical computer vision."',
       image: Kun_yuan,
     },
     {
       name: 'Keqi Chen',
-      link: 'https://www.linkedin.com/in/keqichen-2bb510304/',
+      linkedin_link: 'https://www.linkedin.com/in/keqichen-2bb510304/',
+      scholar_link: 'https://scholar.google.com/citations?user=e_uNeAQAAAAJ&hl=en&oi=ao',
       period: '[2023 -- ongoing]',
       thesis: 'Thesis on "Holistic surgical scene analysis from multi-modal operating room data."',
       image: Keqi_chen,
     },
     {
-      name: 'Shi Li',
-      link: 'https://www.linkedin.com/in/shi-li-640140204/',
-      period: '[2024 -- ongoing]',
-      thesis: 'Thesis on "Multi-modality learning from video & text for large scale surgical video analysis."',
-      image: Shi_li,
-    },
-    {
       name: 'Farahdiba Zarin',
-      link: 'https://www.linkedin.com/in/farahdiba-zarin/',
+      linkedin_link: 'https://www.linkedin.com/in/farahdiba-zarin/',
+      scholar_link: 'https://scholar.google.com/citations?user=9ly0UPIAAAAJ&hl=en&oi=ao',
       period: '[2023 -- ongoing]',
       thesis: 'Thesis on "Self-supervised video representation learning for surgical video analysis."',
       image: Farahdiba_zarin,
+    },    
+    {
+      name: 'Shi Li',
+      linkedin_link: 'https://www.linkedin.com/in/shi-li-640140204/',  
+      period: '[2024 -- ongoing]',
+      thesis: 'Thesis on "Multi-modality learning from video & text for large scale surgical video analysis."',
+      image: Shi_li,
     },
   ];
--- src/Components/Content/Supervision.jsx
+++ src/Components/Content/Supervision.jsx
@@ -4,6 +4,9 @@ import 'aos/dist/aos.css';
 import { phdCandidates } from '../../All_Lists/PhDCandidates';
 import { researchInternships } from '../../All_Lists/ResearchCandidates';
 import { Helmet } from 'react-helmet';
+import { FaGoogleScholar } from "react-icons/fa6";
+import { FaLinkedinIn } from 'react-icons/fa';
+
 const CandidatesAndInterns = () => {
   useEffect(() => {
     AOS.init({ duration: 800, easing: 'ease-in-out', once: true });
@@ -16,106 +19,123 @@ const CandidatesAndInterns = () => {
         <meta name="description" content="Explore my supervision and guidance." />
         <meta name="keywords" content="supervision, guidance, mentorship, academic" />
       </Helmet>
-    <section id="Supervision" className="bg-white dark:bg-slate-900 text-black dark:text-white py-20 px-10 lg:px-20">
-    <h1 className="text-[52px] font-semibold mb-20 leading-normal uppercase text-white-500">
-      Supervision
-      <span className="text-[15px] block normal-case font-medium text-gray-400 dark:text-gray-400">
-        (along with Prof. Nicolas Padoy)
-      </span>
-    </h1>
-      <div className="space-y-8">
-        <div>
-          <h2
-            className="text-2xl font-semibold mb-6 text-white-600 dark:text-white-400"
-          >
-            PhD Candidates
-          </h2>
-          <div className="space-y-4">
-            {phdCandidates.map((candidate, index) => (
-              <div
-                key={index}
-                className="flex flex-col sm:flex-row justify-between border-b border-gray-200 dark:border-gray-700 pb-4 hover:bg-gray-50 dark:hover:bg-slate-800 transition-all duration-300 rounded-lg p-3"
-              >
-                <span className="text-gray-600 dark:text-gray-400 font-medium w-48 sm:w-64 mb-2 sm:mb-0 whitespace-nowrap">
-                  {candidate.period}
-                </span>
-                {/* Clickable image */}
-                <a
-                  href={candidate.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex-shrink-0 w-12 h-12 sm:w-16 sm:h-16 rounded-full overflow-hidden border-2 border-gray-200 dark:border-gray-700 hover:border-blue-500 transition-all duration-300"
+      <section id="Supervision" className="bg-white dark:bg-slate-900 text-black dark:text-white py-20 px-10 lg:px-20">
+        <h1 className="text-[52px] font-semibold mb-20 leading-normal uppercase text-white-500">
+          Supervision
+          <span className="text-[15px] block normal-case font-medium text-gray-400 dark:text-gray-400">
+            (along with Prof. Nicolas Padoy)
+          </span>
+        </h1>
+        <div className="space-y-8">
+          <div>
+            <h2 className="text-2xl font-semibold mb-6 text-white-600 dark:text-white-400">
+              PhD Candidates
+            </h2>
+            <div className="space-y-4">
+              {phdCandidates.map((candidate, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col sm:flex-row justify-between border-b border-gray-200 dark:border-gray-700 pb-4 hover:bg-gray-50 dark:hover:bg-slate-800 transition-all duration-300 rounded-lg p-3"
                 >
-                  <img
-                    src={candidate.image}
-                    alt={candidate.name}
-                    className="w-full h-full object-cover"
-                  />
-                </a>
-                <div className="flex-1 ml-4">
+                  <span className="text-gray-600 dark:text-gray-400 font-medium w-48 sm:w-64 mb-2 sm:mb-0 whitespace-nowrap">
+                    {candidate.period}
+                  </span>
                   <a
-                    href={candidate.link}
+                    href={candidate.linkedin_link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
+                    className="flex-shrink-0 w-12 h-12 sm:w-16 sm:h-16 rounded-full overflow-hidden border-2 border-gray-200 dark:border-gray-700 hover:border-blue-500 transition-all duration-300"
                   >
-                    {candidate.name}
+                    <img
+                      src={candidate.image}
+                      alt={candidate.name}
+                      className="w-full h-full object-cover"
+                    />
                   </a>
-                  <p className="text-gray-600 dark:text-gray-400 mt-1">
-                    {candidate.thesis}
-                  </p>
+                  <div className="flex-1 ml-4">
+                    <a
+                      href={candidate.linkedin_link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
+                    >
+                      {candidate.name}
+                    </a>
+                    <p className="text-gray-600 dark:text-gray-400 mt-1">
+                      {candidate.thesis}
+                    </p>
+                    <div className="flex space-x-2 mt-2">
+                      <a href={candidate.linkedin_link} target="_blank" rel="noopener noreferrer">
+                        <FaLinkedinIn className="text-blue-600 hover:text-blue-800" size={16} />
+                      </a>
+                      {/* Conditionally render Google Scholar icon if scholar_link exists */}
+                      {candidate.scholar_link && (
+                        <a href={candidate.scholar_link} target="_blank" rel="noopener noreferrer">
+                          <FaGoogleScholar className="text-cyan-600 hover:text-green-800" size={16} />
+                        </a>
+                      )}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
 
-        <div>
-          <h2
-            className="text-2xl font-semibold mb-6 text-white-600 dark:text-white-400"
-          >
-            Research Internships
-          </h2>
-          <div className="space-y-4">
-            {researchInternships.map((intern, index) => (
-              <div
-                key={index}
-                className="flex flex-col sm:flex-row justify-between border-b border-gray-200 dark:border-gray-700 pb-4 hover:bg-gray-50 dark:hover:bg-slate-800 transition-all duration-300 rounded-lg p-3"
-              >
-                <span className="text-gray-600 dark:text-gray-400 font-medium w-48 sm:w-64 mb-2 sm:mb-0 whitespace-nowrap">
-                  {intern.period}
-                </span>
-                <a
-                  href={intern.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex-shrink-0 w-12 h-12 sm:w-16 sm:h-16 rounded-full overflow-hidden border-2 border-gray-200 dark:border-gray-700 hover:border-blue-500 transition-all duration-300"
+          <div>
+            <h2 className="text-2xl font-semibold mb-6 text-white-600 dark:text-white-400">
+              Research Internships
+            </h2>
+            <div className="space-y-4">
+              {researchInternships.map((intern, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col sm:flex-row justify-between border-b border-gray-200 dark:border-gray-700 pb-4 hover:bg-gray-50 dark:hover:bg-slate-800 transition-all duration-300 rounded-lg p-3"
                 >
-                  <img
-                    src={intern.image}
-                    alt={intern.name}
-                    className="w-full h-full object-cover"
-                  />
-                </a>
-                <div className="flex-1 ml-4">
+                  <span className="text-gray-600 dark:text-gray-400 font-medium w-48 sm:w-64 mb-2 sm:mb-0 whitespace-nowrap">
+                    {intern.period}
+                  </span>
                   <a
-                    href={intern.link}
+                    href={intern.linkedin_link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
+                    className="flex-shrink-0 w-12 h-12 sm:w-16 sm:h-16 rounded-full overflow-hidden border-2 border-gray-200 dark:border-gray-700 hover:border-blue-500 transition-all duration-300"
                   >
-                    {intern.name}
+                    <img
+                      src={intern.image}
+                      alt={intern.name}
+                      className="w-full h-full object-cover"
+                    />
                   </a>
-                  <p className="text-gray-600 dark:text-gray-400 mt-1">
-                    {intern.description}
-                  </p>
+                  <div className="flex-1 ml-4">
+                    <a
+                      href={intern.linkedin_link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
+                    >
+                      {intern.name}
+                    </a>
+                    <p className="text-gray-600 dark:text-gray-400 mt-1">
+                      {intern.description}
+                    </p>
+                    <div className="flex space-x-2 mt-2">
+                      <a href={intern.linkedin_link} target="_blank" rel="noopener noreferrer">
+                        <FaLinkedinIn className="text-blue-600 hover:text-blue-800" size={16} />
+                      </a>
+                      {/* Conditionally render Google Scholar icon if scholar_link exists */}
+                      {intern.scholar_link && (
+                        <a href={intern.scholar_link} target="_blank" rel="noopener noreferrer">
+                          <FaGoogleScholar className="text-cyan-600 hover:text-green-800" size={16} />
+                        </a>
+                      )}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
